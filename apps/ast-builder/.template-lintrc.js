901b23c104779b81a77fd339191de5e08e8011a1
Rajasegar <rajasegar.c@gmail.com>
2022-05-23 15:04:15 +0530

    chore: removing ast-builder app

--- apps/ast-builder/.template-lintrc.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  extends: 'octane',
-  rules: {
-    'no-curly-component-invocation': false,
-    'no-action': false,
-  },
-};
--- apps/ast-builder/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-# ast-builder
-
-## 0.0.1
-
-### Patch Changes
-
-- Updated dependencies [987099d]
-  - ast-node-builder@4.1.0
